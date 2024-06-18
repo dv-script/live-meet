a8f0188434baa45679d84fd8e506da9c33ca2a29
Davi Hasson Castro <davihasson@gmail.com>
2024-06-18 12:08:36 -0300

    chore: authenticate user

---
+++ src/app/_actions/authenticate-user.ts
@@ -0,0 +1,66 @@
+"use server";
+import { signIn } from "@/app/auth/providers";
+import { AuthError } from "next-auth";
+import { z } from "zod";
+
+const authenticateUserSchema = z.object({
+  email: z.string().email({ message: "Por favor, insira um e-mail válido" }),
+  password: z
+    .string()
+    .min(8, { message: "A senha deve ter no mínimo 8 caracteres" }),
+});
+
+export type State = {
+  errors?: {
+    email?: string[];
+    password?: string[];
+  };
+  message?: string;
+  success?: boolean;
+};
+
+export async function authenticateUser(_prevState: State, formData: FormData) {
+  const validatedFields = authenticateUserSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Por favor, preencha corretamente os campos.",
+    };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: "/booking",
+    });
+    return {
+      success: true,
+      message: "Você foi logado com sucesso.",
+    };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            message:
+              "E-mail ou senha estão inválidos. Por favor, tente novamente.",
+            success: false,
+          };
+        default:
+          return {
+            message: "Algo deu errado. Por favor, tente novamente.",
+            success: false,
+          };
+      }
+    }
+
+    throw error;
+  }
+}
---
+++ src/app/auth/sign-in/_components/sign-in-form.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Input } from "@ui/input";
+import { Button } from "@ui/button";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import { authenticateUser } from "@actions/authenticate-user";
+import { SubmitButton } from "@/app/_components/submit-button";
+import { FormError } from "@/app/_components/form-error";
+import { FormField } from "@/app/_components/form-field";
+
+export function SignInForm() {
+  const initialState = { message: "", errors: {} };
+  const [state, dispatch] = useFormState(authenticateUser, initialState);
+
+  return (
+    <form action={dispatch} className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4">
+          <FormField
+            label="E-mail"
+            name="email"
+            placeholder="Digite sua conta de e-mail"
+            errorMessages={state.errors?.email}
+          />
+          <FormField
+            label="Senha"
+            name="password"
+            placeholder="Digite sua senha"
+            type="password"
+            errorMessages={state.errors?.password}
+          />
+          {state.success === false && (
+            <FormError errorMessage={state.message} />
+          )}
+        </div>
+        <div className="flex justify-end">
+          <Button asChild variant="link" className="text-sm p-0 h-fit">
+            <Link href="/">Esqueceu sua senha?</Link>
+          </Button>
+        </div>
+      </div>
+      <SubmitButton type="submit">Entrar</SubmitButton>
+    </form>
+  );
+}
