06e75537d442087d71c10980cbd42807422ea3f3
Davi Hasson Castro <davihasson@gmail.com>
2024-06-18 12:09:50 -0300

    feat: new form error component

---
+++ src/app/_components/form-error.tsx
@@ -0,0 +1,16 @@
+import { TriangleAlert } from "lucide-react";
+
+interface FormErrorProps {
+  errorMessage?: string;
+}
+
+export function FormError({ errorMessage }: FormErrorProps) {
+  return (
+    <div className="flex gap-2 p-2 items-center bg-red-500/10 rounded-xl">
+      <span className="text-red-500">
+        <TriangleAlert className="w-4 h-4 text-sm" />
+      </span>
+      <p className="text-red-500 text-sm">{errorMessage}</p>
+    </div>
+  );
+}
