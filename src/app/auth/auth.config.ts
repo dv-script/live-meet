35772e9a92bde2ac58934aa8ef171cb9181c79ee
Davi Hasson Castro <davihasson@gmail.com>
2024-06-18 12:12:07 -0300

    feat: adding next auth config

---
+++ src/app/auth/auth.config.ts
@@ -0,0 +1,66 @@
+import { Departament, Role } from "@prisma/client";
+import { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  providers: [],
+  useSecureCookies: process.env.NODE_ENV === "production",
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const userLoggedIn = !!auth?.user;
+      const isHomeRoute = nextUrl.pathname === "/";
+      const isAuthRoutes = nextUrl.pathname.startsWith("/auth");
+      const authOrHomeRoutes = isHomeRoute || isAuthRoutes;
+
+      const isAdminRoutes = nextUrl.pathname.startsWith("/admin");
+      const isAdmin = auth?.user?.role === "ADMIN";
+
+      if (userLoggedIn && isHomeRoute) {
+        return Response.redirect(new URL("/booking", nextUrl));
+      }
+
+      if (!userLoggedIn && !authOrHomeRoutes) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
+
+      if (userLoggedIn && authOrHomeRoutes) {
+        return Response.redirect(new URL("/booking", nextUrl));
+      }
+
+      if (isAdminRoutes && !isAdmin) {
+        return false;
+      }
+
+      return true;
+    },
+    session({ session, token }) {
+      if (token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.role = token.role as Role;
+        session.user.department = token.department as Departament;
+      }
+
+      return session;
+    },
+    jwt({ token, user }) {
+      if (!user) return token;
+
+      if (user) {
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+        token.department = user.department;
+      }
+
+      return token;
+    },
+  },
+} as NextAuthConfig;
