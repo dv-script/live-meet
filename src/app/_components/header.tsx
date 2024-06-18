c983c686175c3b1ffe3ee986a8d5ac13050cf672
Davi Hasson Castro <davihasson@gmail.com>
2024-06-18 12:10:24 -0300

    feat: new header component

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,109 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { HomeIcon, LogInIcon, MenuIcon, Shield } from "lucide-react";
+import LogoImage from "@public/logo.png";
+import { Separator } from "./ui/separator";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { auth } from "../auth/providers";
+
+export async function Header() {
+  const session = await auth();
+
+  return (
+    <div className="mx-auto flex items-center gap-2 justify-between p-2 max-w-screen-xl">
+      <Link href="/" className="relative h-16 w-16">
+        <Image
+          src={LogoImage.src}
+          alt="Cazé Logo"
+          fill
+          sizes="100%"
+          className="object-contain"
+        />
+      </Link>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            size="icon"
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+          <div className="flex flex-col gap-2 pt-6">
+            {session ? (
+              <div className="flex gap-2 items-center pb-4">
+                <Avatar>
+                  <AvatarFallback>{session.user?.name![0]}</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <h2 className="font-semibold">{session.user?.name}</h2>
+                  <span className="text-xs text-muted-foreground">
+                    {session.user?.email}
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="flex justify-between items-center gap-2">
+                  <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                  <SheetClose asChild>
+                    <Button asChild size="icon-xs">
+                      <Link href="/auth/sign-in">
+                        <LogInIcon size={14} />
+                      </Link>
+                    </Button>
+                  </SheetClose>
+                </div>
+                <div className="py-3">
+                  <Separator />
+                </div>
+              </>
+            )}
+            <div className="flex flex-col gap-2">
+              <SheetClose asChild>
+                <Button
+                  variant="ghost"
+                  className="w-full justify-start gap-3 items-center rounded-full text-sm font-normal"
+                  asChild
+                >
+                  <Link href="/">
+                    <HomeIcon size={16} />
+                    <span className="block">Ínicio</span>
+                  </Link>
+                </Button>
+              </SheetClose>
+              {session?.user?.role === "ADMIN" && (
+                <SheetClose asChild>
+                  <Button
+                    asChild
+                    variant="ghost"
+                    className="w-full justify-start gap-3 items-center rounded-full text-sm font-normal"
+                  >
+                    <Link href="/admin">
+                      <Shield size={16} />
+                      <span className="block">Admin</span>
+                    </Link>
+                  </Button>
+                </SheetClose>
+              )}
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
