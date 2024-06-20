ed5ed38ebaa1f943589f71fb7e0d24c62a173ff9
dv-script <davihasson@gmail.com>
2024-06-20 19:59:31 -0300

    feat: new page to admin users from application

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/app/_lib/prisma";
+import { AddNewUserDialog } from "./_components/_add-new-user/add-new-user-dialog";
+import { UsersTable } from "./_components/_users-table/users-table";
+
+export default async function Page() {
+  const users = await db.user.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      department: true,
+      role: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return (
+    <div className="px-4 py-3 flex flex-col gap-5 max-w-screen-xl mx-auto">
+      <div className="flex flex-col">
+        <AddNewUserDialog />
+        <UsersTable usersData={users} />
+      </div>
+    </div>
+  );
+}
