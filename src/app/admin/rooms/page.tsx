bec2557bef91532b04560fa5c1bbd4ff6b08df0f
dv-script <davihasson@gmail.com>
2024-06-21 16:47:03 -0300

    feat: new room admin page

--- src/app/admin/rooms/page.tsx
+++ src/app/admin/rooms/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/app/_lib/prisma";
 import { AddNewRoomDialog } from "./_components/_add-new-room/add-new-room-dialog";
+import { RoomsTable } from "./_components/_rooms-table/rooms-table";
 
 export default async function Page() {
   const rooms = await db.room.findMany({
@@ -12,6 +13,7 @@ export default async function Page() {
     <div className="px-4 py-3 flex flex-col gap-5 max-w-screen-xl mx-auto">
       <div className="flex flex-col">
         <AddNewRoomDialog />
+        <RoomsTable roomsData={rooms} />
       </div>
     </div>
   );
