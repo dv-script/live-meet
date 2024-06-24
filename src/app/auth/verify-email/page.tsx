c8539937f6b9726ea78a070f3cf52f7d86369126
dv-script <davihasson@gmail.com>
2024-06-24 12:04:38 -0300

    fix: ensure that the verify email form is included in the suspense

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import { VerifyEmailForm } from "./_components/verify-email-form";
+import { Loading } from "@/app/_components/loading";
 
 export default async function Page() {
   return (
     <div className="px-4 py-8 flex flex-col gap-12 max-w-screen-xl mx-auto">
-      <VerifyEmailForm />
+      <Suspense fallback={<Loading />}>
+        <VerifyEmailForm />
+      </Suspense>
     </div>
   );
 }
