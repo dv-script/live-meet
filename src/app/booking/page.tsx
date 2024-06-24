f45b0a8e860342a8c8eae428f523f88812bc941b
dv-script <davihasson@gmail.com>
2024-06-24 12:05:14 -0300

    feat: new room list to schedule a meet room

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,11 +1,38 @@
+import { db } from "../_lib/prisma";
 import { auth } from "../auth/providers";
+import { RoomList } from "./_components/room-list";
 
 export default async function Page() {
   const session = await auth();
+  const rooms = await db.room.findMany({
+    include: {
+      bookings: true,
+    },
+    orderBy: {
+      location: "asc",
+    },
+  });
+
+  const userId = session?.user?.id;
 
   return (
-    <>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-    </>
+    <div className="px-4 py-8 flex flex-col gap-12 max-w-screen-xl mx-auto">
+      <RoomList userId={userId} rooms={rooms} />
+      <div className="grid gap-4">
+        <div className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg">
+          <h2 className="text-xl font-bold mb-2">Upcoming Meetings</h2>
+          <div className="grid gap-4">
+            <div className="flex items-center justify-between">
+              <div>
+                <h3 className="font-semibold">Team Brainstorming</h3>
+                <p className="text-gray-500 dark:text-gray-400">
+                  Conference Room A - 11:00 AM
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
