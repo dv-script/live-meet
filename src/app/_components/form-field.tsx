7bc25b52a276a942d62ac33b85a59b19ec2a5b47
dv-script <davihasson@gmail.com>
2024-06-30 21:27:53 -0300

    fix: ensure the props is correct

--- src/app/_components/form-field.tsx
+++ src/app/_components/form-field.tsx
@@ -3,29 +3,19 @@ import { Label } from "./ui/label";
 
 interface FormFieldProps {
   label: string;
-  name: string;
-  placeholder: string;
-  type?: string;
-  defaultValue?: string | number;
   errorMessages?: string[];
 }
 
 export function FormField({
   label,
-  name,
-  placeholder,
-  type,
-  defaultValue,
   errorMessages,
-}: FormFieldProps) {
+  ...props
+}: FormFieldProps & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <Label>{label}</Label>
       <Input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        defaultValue={defaultValue}
+        {...props}
         className={
           errorMessages && errorMessages.length > 0 ? "border-red-500" : ""
         }
