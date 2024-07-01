62da4f7e043e591de7e17bf22f5418ac0375d709
dv-script <davihasson@gmail.com>
2024-07-01 20:44:01 -0300

    refactor: adding ref on form to reset after success

--- src/app/settings/_components/change-password-form.tsx
+++ src/app/settings/_components/change-password-form.tsx
@@ -3,23 +3,20 @@
 import { changePassword } from "@/app/_actions/change-password";
 import { FormField } from "@/app/_components/form-field";
 import { SubmitButton } from "@/app/_components/submit-button";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useFormState } from "react-dom";
 import { toast } from "sonner";
 
 export function ChangePasswordForm() {
   const initialState = { errors: {}, message: "" };
   const [state, dispatch] = useFormState(changePassword, initialState);
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
     if (state?.success) {
       toast.success(state.message);
-      setCurrentPassword("");
-      setNewPassword("");
-      setPasswordConfirmation("");
+      formRef.current?.reset();
     }
 
     if (!state.success && state.message) {
@@ -29,14 +26,12 @@ export function ChangePasswordForm() {
   }, [state]);
 
   return (
-    <form action={dispatch} className="flex flex-col gap-3">
+    <form ref={formRef} action={dispatch} className="flex flex-col gap-3">
       <FormField
         label="Senha atual"
         name="currentPassword"
         type="password"
         placeholder="Insira sua senha atual"
-        value={currentPassword}
-        onChange={(e) => setCurrentPassword(e.target.value)}
         errorMessages={state.errors?.currentPassword}
       />
       <FormField
@@ -44,8 +39,6 @@ export function ChangePasswordForm() {
         name="newPassword"
         type="password"
         placeholder="Insira sua nova senha"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
         errorMessages={state.errors?.newPassword}
       />
       <FormField
@@ -53,8 +46,6 @@ export function ChangePasswordForm() {
         name="passwordConfirmation"
         type="password"
         placeholder="Confirme sua nova senha"
-        value={passwordConfirmation}
-        onChange={(e) => setPasswordConfirmation(e.target.value)}
         errorMessages={state.errors?.passwordConfirmation}
       />
       <SubmitButton>Alterar senha</SubmitButton>
